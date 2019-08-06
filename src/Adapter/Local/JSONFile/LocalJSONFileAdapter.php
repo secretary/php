e2a97bbe472fda947a4d7815a6d9c61204a5626b
Thomas Picquet <thomas@sctr.net>
2019-08-06 09:21:08 -0700

    fixed style and added .editorconfig

--- src/Adapter/Local/JSONFile/LocalJSONFileAdapter.php
+++ src/Adapter/Local/JSONFile/LocalJSONFileAdapter.php
@@ -13,7 +13,6 @@ namespace Secretary\Adapter\AWS\SecretsManager;
 
 use Secretary\Adapter\AbstractAdapter;
 use Secretary\Exception\SecretNotFoundException;
-use Secretary\Helper\ArrayHelper;
 use Secretary\Secret;
 
 /**
@@ -23,137 +22,137 @@ use Secretary\Secret;
  */
 class LocalJSONFileAdapter extends AbstractAdapter
 {
-	/**
-	 * @param Secret   $secret
-	 * @param Secret[] $secrets
-	 *
-	 * @return array
-	 */
-	private static function updateValue(Secret $secret, array $secrets): array
-	{
-		$keys  = array_column($secrets, 'key');
-		$index = array_search($secret->getKey(), $keys, true);
-		if ($index === false || $index === null) {
-			$secrets[] = $secret;
-		} else {
-			$secrets[$index] = $secrets[$index]->withValue($secret->getValue());
-		}
+    /**
+     * @param Secret   $secret
+     * @param Secret[] $secrets
+     *
+     * @return array
+     */
+    private static function updateValue(Secret $secret, array $secrets): array
+    {
+        $keys  = array_column($secrets, 'key');
+        $index = array_search($secret->getKey(), $keys, true);
+        if ($index === false || $index === null) {
+            $secrets[] = $secret;
+        } else {
+            $secrets[$index] = $secrets[$index]->withValue($secret->getValue());
+        }
 
-		return $secrets;
-	}
+        return $secrets;
+    }
 
-	/**
-	 * @var string
-	 */
-	private $secretsFile;
+    /**
+     * @var string
+     */
+    private $secretsFile;
 
-	/**
-	 * @var array
-	 */
-	private $jsonOptions;
+    /**
+     * @var array
+     */
+    private $jsonOptions;
 
-	/**
-	 * LocalJSONFileAdapter constructor.
-	 *
-	 * @param array $config
-	 *
-	 * @throws \Exception
-	 */
-	public function __construct(array $config)
-	{
-		if (!isset($config)) {
-			throw new \Exception('Configuration is required.');
-		}
+    /**
+     * LocalJSONFileAdapter constructor.
+     *
+     * @param array $config
+     *
+     * @throws \Exception
+     */
+    public function __construct(array $config)
+    {
+        if (!isset($config)) {
+            throw new \Exception('Configuration is required.');
+        }
 
-		if (!isset($config['file'])) {
-			throw new \Exception('`file` is a required config.');
-		}
-		if (isset($config['jsonOptions'])) {
-			if (!is_array($config['jsonOptions'])) {
-				throw new \Exception('`jsonOptions` must be an array');
-			}
-		} else {
-			$config['jsonOptions'] = [JSON_PRETTY_PRINT];
-		}
+        if (!isset($config['file'])) {
+            throw new \Exception('`file` is a required config.');
+        }
+        if (isset($config['jsonOptions'])) {
+            if (!is_array($config['jsonOptions'])) {
+                throw new \Exception('`jsonOptions` must be an array');
+            }
+        } else {
+            $config['jsonOptions'] = [JSON_PRETTY_PRINT];
+        }
 
-		$this->secretsFile = $config['file'];
-		$this->jsonOptions = $config['jsonOptions'];
-	}
+        $this->secretsFile = $config['file'];
+        $this->jsonOptions = $config['jsonOptions'];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 * @throws \Exception
-	 */
-	public function getSecret(string $key, ?array $options = []): Secret
-	{
-		$secrets = $this->loadSecrets();
-		$keys    = array_column($secrets, 'key');
-		$index   = array_search($key, $keys, true);
-		if ($index === false || $index === null) {
-			throw new SecretNotFoundException($key);
-		}
+    /**
+     * {@inheritdoc}
+     * @throws SecretNotFoundException
+     */
+    public function getSecret(string $key, ?array $options = []): Secret
+    {
+        $secrets = $this->loadSecrets();
+        $keys    = array_column($secrets, 'key');
+        $index   = array_search($key, $keys, true);
+        if ($index === false || $index === null) {
+            throw new SecretNotFoundException($key);
+        }
 
-		return $secrets[$index];
-	}
+        return $secrets[$index];
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function putSecret(Secret $secret, ?array $options = []): Secret
-	{
-		$secrets = LocalJSONFileAdapter::updateValue($secret, $this->loadSecrets());
-		$this->saveSecrets($secrets);
+    /**
+     * {@inheritdoc}
+     */
+    public function putSecret(Secret $secret, ?array $options = []): Secret
+    {
+        $secrets = LocalJSONFileAdapter::updateValue($secret, $this->loadSecrets());
+        $this->saveSecrets($secrets);
 
-		return $secret;
-	}
+        return $secret;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function deleteSecretByKey(string $key, ?array $options = []): void
-	{
-		$secrets = $this->loadSecrets();
-		$keys    = array_column($secrets, 'key');
-		$index   = array_search($key, $keys, true);
-		if ($index === false || $index === null) {
-			throw new SecretNotFoundException($key);
-		}
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteSecretByKey(string $key, ?array $options = []): void
+    {
+        $secrets = $this->loadSecrets();
+        $keys    = array_column($secrets, 'key');
+        $index   = array_search($key, $keys, true);
+        if ($index === false || $index === null) {
+            throw new SecretNotFoundException($key);
+        }
 
-		array_splice($secrets, $index, 1);
-		$this->saveSecrets($secrets);
-	}
+        array_splice($secrets, $index, 1);
+        $this->saveSecrets($secrets);
+    }
 
-	/**
-	 * {@inheritdoc}
-	 * @throws SecretNotFoundException
-	 */
-	public function deleteSecret(Secret $secret, ?array $options = []): void
-	{
-		$this->deleteSecretByKey($secret->getKey(), $options);
-	}
+    /**
+     * {@inheritdoc}
+     * @throws SecretNotFoundException
+     */
+    public function deleteSecret(Secret $secret, ?array $options = []): void
+    {
+        $this->deleteSecretByKey($secret->getKey(), $options);
+    }
 
-	/**
-	 * @return Secret[]
-	 * @throws \Exception
-	 */
-	private function loadSecrets(): array
-	{
-		$contents = file_get_contents($this->secretsFile);
-		$json     = json_decode($contents, true, 512, [JSON_THROW_ON_ERROR]);
+    /**
+     * @return Secret[]
+     * @throws \Exception
+     */
+    private function loadSecrets(): array
+    {
+        $contents = file_get_contents($this->secretsFile);
+        $json     = json_decode($contents, true, 512, [JSON_THROW_ON_ERROR]);
 
-		return array_map(
-			function (array $secret) {
-				return new Secret($secret['key'], $secret['value'], $secret['metadata'] ?? null);
-			},
-			$json
-		);
-	}
+        return array_map(
+            function (array $secret) {
+                return new Secret($secret['key'], $secret['value'], $secret['metadata'] ?? null);
+            },
+            $json
+        );
+    }
 
-	/**
-	 * @param array $secrets
-	 */
-	private function saveSecrets(array $secrets)
-	{
-		file_put_contents($this->secretsFile, json_encode($secrets, $this->jsonOptions));
-	}
+    /**
+     * @param array $secrets
+     */
+    private function saveSecrets(array $secrets)
+    {
+        file_put_contents($this->secretsFile, json_encode($secrets, $this->jsonOptions));
+    }
 }
